502edbf4b4453e6893bde3d24ca77c94b6b76ff6
Azizbek Gaybullaev <azizgaybullaev@mail.ru>
2020-01-21 19:47:30 +0500

    modified

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 ## Weather Data
 
+This is a script to collect weather information from Dark Sky https://darksky.net/dev
+
+In order to run this script firstly clone your own system with this url
+
 ```
-dfdfdfdfdfdfdfdfdfdfdddddddddddddddddddddddddddddddddd
+https://github.com/goaziz/weather_data.git
 ```
+
+The next step: create virtual environment in your working space by this command ``python3.x -m venv venv`` then install all dependencies from `pip install -r requirements.txt` file.
+
+Now you're all set. This is the useful commands to run the script
+
+```
+python main.py run &
+```
+this command does background task where data is collected and saved into the database every 1 minute
+
+```
+python main.py city_id id
+```
+
+this command collects weather information for the specified city which you should enter from 1 to 5 numbers instead of id  
+
+```
+python main.py convert cities.csv
+```
+
+this exports all weather table information to csv
--- main.py
+++ main.py
@@ -1,79 +1,75 @@
 import requests
 import sqlite3
 import csv
-import daemon
 import fire
 import time
-import datetime
 
 API_KEY = '85b6c7a69e371c6820aa44273521292f'
 
-base_url = 'https://api.darksky.net/forecast/{}/{},{}?exclude=hourly,daily,minutely'
+base_url = 'https://api.darksky.net/forecast/{}/{},{}'
 
 
-# def get_all_information():
-#     try:
-#         database = sqlite3.connect("data.db")
-#         cursor = database.cursor()
-#         sql_select_query = "SELECT * FROM weather;"
-#         cursor.execute(sql_select_query, )
-#         cities = cursor.fetchall()
-#         cursor.close()
-#         database.close()
-#
-#         return cities
-#     except Exception as e:
-#         print(e)
+def get_all_information():
+    """
+    Comment: this does background task which every minute collects information
+    and saves into weather table in the database
+    """
+    database = sqlite3.connect("data.db")
+    cursor = database.cursor()
+    sql_select_query = "SELECT * FROM cities;"
+    cursor.execute(sql_select_query, )
+    cities = cursor.fetchall()
+    for city in cities:
+        lat = city[2]
+        lon = city[3]
+        request_url = base_url.format(
+            API_KEY,
+            lat,
+            lon
+        )
+        result = requests.get(request_url).json()
+        data = {}
+        data['Time'] = result['currently']['time']
+        data['Summary'] = result['currently']['summary']
+        data['Wind Speed'] = result['currently']['windSpeed']
+        data['Temperature'] = result['currently']['temperature']
+        data['uvIndex'] = result['currently']['uvIndex']
+        data['Visibility'] = result['currently']['visibility']
+
+        cursor.execute(
+            "INSERT INTO weather (updated_at, summary, windSpeed, temperature, uvIndex, visibility, city_id) VALUES (?, ?, ?, ?, ?, ?, ?);",
+            (result['currently']['time'], result['currently']['summary'], result['currently']['windSpeed'],
+             result['currently']['temperature'], result['currently']['uvIndex'], result['currently']['visibility'],
+             city[0]))
+
+        database.commit()
 
 
-def get_city_by_id(city_id):
+def get_weather_by_city_id(city_id):
+    """
+    Comment: this retrieves weather information for specified city
+    :param city_id:
+    :return:
+    """
     try:
         database = sqlite3.connect("data.db")
         cursor = database.cursor()
-        sql_select_query = "SELECT * FROM cities where id = ?;"
+        sql_select_query = "SELECT * FROM weather where city_id = ? AND updated_at < DATETIME('now', '-10 minute');"
         cursor.execute(sql_select_query, (city_id,))
-        cities = cursor.fetchall()
-
-        for city in cities:
-            lat = city[2]
-            lon = city[3]
-            request_url = base_url.format(
-                API_KEY,
-                lon,
-                lat
-            )
-            data = requests.get(request_url).json()
-            return data
-
-        cursor.close()
-        database.close()
+        cities_weather = cursor.fetchall()
+        database.commit()
+        return cities_weather
 
     except Exception as e:
-        print(e)
-
-
-def current_weather(city_id):
-    result = get_city_by_id(city_id)
-    data = {}
-    data['Time'] = result['currently']['time']
-    data['Summary'] = result['currently']['summary']
-    data['Wind Speed'] = result['currently']['windSpeed']
-    data['Temperature'] = result['currently']['temperature']
-    data['uvIndex'] = result['currently']['uvIndex']
-    data['Visibility'] = result['currently']['visibility']
-    database = sqlite3.connect("data.db")
-    cursor = database.cursor()
-    cursor.execute(
-        "INSERT INTO weather (time, summary, windSpeed, temperature, uvIndex, visibility) VALUES (?, ?, ?, ?, ?, ?);",
-        (result['currently']['time'], result['currently']['summary'], result['currently']['windSpeed'],
-         result['currently']['temperature'], result['currently']['uvIndex'], result['currently']['visibility']))
-    database.commit()
-    database.close()
-
-    return data
+        return f'Error: {e}'
 
 
 def convert(fname):
+    """
+    Comment: this function export all data from weather table into csv file
+    :param fname:
+    :return:
+    """
     database = sqlite3.connect("data.db")
     cursor = database.cursor()
     cursor.execute("SELECT * FROM weather;")
@@ -89,14 +85,15 @@ def convert(fname):
     return f'{fname} is not expected file type. Try with csv please!'
 
 
-# def run():
-#     with daemon.DaemonContext():
-#         print(get_all_information())
+def run():
+    while True:
+        time.sleep(60)
+        get_all_information()
 
 
 if __name__ == '__main__':
-    # run()
     fire.Fire({
-        'current': current_weather,
-        'convert': convert,
+        'run': run,
+        'city_id': get_weather_by_city_id,
+        'convert': convert
     })
